e804c2cc094bc9b5507891d5578b20f625c36a09
Shaowei <zsw22@vip.qq.com>
2017-11-23 16:17:20 +1100

    add cell color and style

--- js/main.js
+++ js/main.js
@@ -20,4 +20,39 @@ function init() {
             gridCell.css('left', getPosLeft(i, j));
         }
     }
+
+    for (var i = 0; i < 4; i++) {
+        board[i] = new Array();
+        for (var j = 0; j < 4; j++) {
+            board[i][j] = 0;
+        }
+    }
+
+    updateBoardView();
+}
+
+function updateBoardView() {
+    $('.number-cell').remove();
+    for (var i = 0; i < 4; i++) {
+        for (var j = 0; j < 4; j++) {
+            var numberCellDiv = $("div").className("number-cell").attr("id", "number-cell-" + i + "-" + j);
+            $('#grid-container').append(numberCellDiv);
+            var numberOfCell = $("#number-cell-" + i + "-" + j);
+
+            if (board[i][j] === 0) {
+                numberOfCell.css("width", "0px");
+                numberOfCell.css("height", "0px");
+                numberOfCell.css("top", getPosTop(i, j) + 50);
+                numberOfCell.css("left", getPosLeft(i, j) + 50);
+            } else {
+                numberOfCell.css("width", "100px");
+                numberOfCell.css("height", "100px");
+                numberOfCell.css("top", getPosTop(i, j));
+                numberOfCell.css("left", getPosLeft(i, j));
+                numberOfCell.css("background-color", getNumberBackgroundColor(board[i][j]));
+                numberOfCell.css("color", getNumberColor(board[i][j]));
+                numberOfCell.text(board[i][j]);
+            }
+        }
+    }
 }
