48146766b9713772e30663479f7c9f34c5d06db7
Shaowei <zsw22@vip.qq.com>
2017-11-23 16:45:19 +1100

    add init animation function

--- js/animation.js
+++ js/animation.js
@@ -1,3 +1,14 @@
-function showNumberWithAnimation(x, y, num) {
-    
+function showNumberWithAnimation(i, j, num) {
+    var numberOfCell = $("#number-cell-" + i + "-" + j);
+
+    numberOfCell.css("background-color", getNumberBackgroundColor(num));
+    numberOfCell.css("color", getNumberColor(num));
+    numberOfCell.text(num);
+
+    numberOfCell.animate({
+        width: "100px",
+        height: "100px",
+        top: getPosTop(i, j),
+        left: getPosLeft(i, j)
+    }, 50);
 }
