145de2db49f8f2023c1c7aa8c59e7ca1218bd459
Shaowei <zsw22@vip.qq.com>
2017-11-23 15:49:09 +1100

    add grid container

--- js/main.js
+++ js/main.js
@@ -1,2 +1,23 @@
 var board = new Array();
 var score = 0;
+
+$(document).ready(function () {
+    newGame();
+});
+
+function newGame() {
+    //init the grid container
+    init();
+    //add two random numbers in random cell
+
+}
+
+function init() {
+    for (var i = 0; i < 4; i++) {
+        for (var j = 0; j < 4; j++) {
+            var gridCell = $("#grid-cell-" + i + "-" + j);
+            gridCell.css('top', getPosTop(i, j));
+            gridCell.css('left', getPosLeft(i, j));
+        }
+    }
+}
